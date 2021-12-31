5954a6b012639bb0d650b6f5a567074e7bbafec2
lam <ngtunglam26899@gmail.com>
2021-12-31 20:34:49 +0700

    change little bit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu , Divider} from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-import { Link } from "react-router-dom";
+import { Link , Outlet } from "react-router-dom";
 
 export default function App() {
   const { SubMenu } = Menu;
@@ -14,9 +14,12 @@ export default function App() {
   <Header className="header">
     <div className="logo" />
     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>      
+    
       <Menu.Item key="1"><Link to="/home">Home</Link></Menu.Item>
-      <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
-      <Menu.Item key="3">Log Out</Menu.Item>
+      <Divider orientation="right" >
+        <Menu.Item key="2"><Link to="/login">Login</Link></Menu.Item>
+        <Outlet/>
+      </Divider>
     </Menu>
   </Header>
   <Content style={{ padding: '0 50px' }}>
